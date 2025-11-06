0ce4ca758af4fe1ea11b328272dd82bf52d6975a
yyoshikai <yyoshikai12@gmail.com>
2025-11-06 18:08:20 +0900

    merge_stat_log

--- zinc/251030/ZINC20-2D/merge_stat.py
+++ zinc/251030/ZINC20-2D/merge_stat.py
@@ -1,11 +1,11 @@
 import sys, os, pickle
 from argparse import ArgumentParser
 from collections import Counter
+import psutil
 import numpy as np
 WORKDIR = os.environ.get('WORKDIR', "/workspace")
 sys.path += [f"{WORKDIR}/cplm", f"{WORKDIR}/github/moses"]
 from src.utils.logger import get_logger, add_file_handler, log_git_hash
-
 
 parser = ArgumentParser()
 parser.add_argument('--size', type=int, required=True)
@@ -17,13 +17,20 @@ logger = get_logger(stream=True)
 add_file_handler(logger, "raw/merge_stat.log")
 log_git_hash(logger)
 
+def get_mem():
+    mem = psutil.virtual_memory()
+    return f"{mem.used/2**30:.03f}GB/{mem.total/2**30:.03f}"
+
 for split in ['test', 'test_scaffolds']:
     stat = {}
 
     # FCD
+    logger.info(f'[FCD] started. {get_mem()}')
     fcd_predictions = []
-    for rank in range(args.sample_size):
+    for i, rank in enumerate(range(args.sample_size), 1):
         fcd_predictions.append(np.load(f"raw/compute_stat_sample/{split}/{rank}/fcd_predictions.npy"))
+        if i % 50 == 0:
+            logger.info(f'[FCD] loaded {i} {get_mem()}')
     fcd_predictions = np.concatenate(fcd_predictions, axis=0)
     stat['FCD'] = {
         'mu': fcd_predictions.mean(0), 
@@ -31,34 +38,46 @@ for split in ['test', 'test_scaffolds']:
     }
 
     # SNN
+    logger.info(f'[SNN] started. {get_mem()}')
     fps = []
-    for rank in range(args.size):
+    for i, rank in enumerate(range(args.size), 1):
         fps.append(np.load(f"raw/compute_stat/{split}/{rank}/fps.npy"))
+        if i % 50 == 0:
+            logger.info(f'[SNN] loaded {i} {get_mem()}')
     fps = np.concatenate(fps, axis=0)
     stat['SNN'] = {'fps': fps}
 
     # Frag
+    logger.info(f'[Frag] started. {get_mem()}')
     counter = Counter()
-    for rank in range(args.size):
+    for i, rank in enumerate(range(args.size), 1):
         with open(f"raw/compute_stat/{split}/{rank}/frag_counter.pkl", 'rb') as f:
             counter0 = pickle.load(f)
         counter.update(counter0)
+        if i % 50 == 0:
+            logger.info(f'[Frag] loaded {i} {get_mem()}')
     stat['Frag'] = {'frag': counter}
 
     # Scaf
+    logger.info(f"[Scaf] started.")
     counter = Counter()
-    for rank in range(args.size):
+    for i, rank in enumerate(range(args.size), 1):
         with open(f"raw/compute_stat/{split}/{rank}/scaf_counter.pkl", 'rb') as f:
             counter0 = pickle.load(f)
         counter.update(counter0)
+        if i % 50 == 0:
+            logger.info(f'[Scaf] loaded {i} {get_mem()}')
     stat['Scaf'] = {'scaf': counter}
 
     # wassertain
     for name in ['logP', 'SA', 'QED', 'weight']:
+        logger.info(f"[{name}] started. {get_mem()}")
         value = []
-        for rank in range(args.sample_size):
+        for i, rank in enumerate(range(args.sample_size)):
             with open(f"raw/compute_stat_sample/{split}/{rank}/{name}.pkl", 'rb') as f:
                 value += pickle.load(f)
+            if i % 50 == 0:
+                logger.info(f'[{name}] loaded {i} {get_mem()}')
         stat[name] = {'values': value}
 
     with open(f"stat/{split}.pkl", 'wb') as f:
